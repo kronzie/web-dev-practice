3f82230d4e4724b2551602d0cd75c61922db6131
kronzie <vyshakhnambiar11@gmail.com>
2025-01-09 22:54:00 +0530

    Setup tailwind

---
+++ 04 react/01 book-ecom-component/src/book.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Book = () => {
+  return (
+    <div>
+        <p className="text-white" >Hi</p>
+    </div>
+  )
+}
+
+export default Book;
---
+++ 04 react/01 book-ecom-component/src/booklist.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Book from './book'
+const BookList = () => {
+  return (
+    <div>
+        <Book/>
+        <p className="">Hi</p>
+    </div>
+  )
+}
+
+export default BookList;
